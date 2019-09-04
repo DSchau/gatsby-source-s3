59e41a1783400b46945bb82e6459df35491c2112
Ulisses Albuquerque <ulisses.albuquerque@latitudefinancial.com>
2019-09-04 13:25:12 +1000

    Document support for sessionToken/AWS_SESSION_TOKEN explicitly

--- src/plugin-options.js
+++ src/plugin-options.js
@@ -6,11 +6,13 @@ export const schema = yup.object().shape({
     .shape({
       accessKeyId: yup.string().required(),
       secretAccessKey: yup.string().required(),
+      sessionToken: yup.string(),
       region: yup.string(),
     })
     .default(() => ({
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      sessionToken: process.env.AWS_SESSION_TOKEN,
       region: process.env.AWS_REGION,
     })),
   buckets: yup.array().required(),
