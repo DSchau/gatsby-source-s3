f6c2f3d83609d4fb0aadd54ec842aeca74ecbaf0
Felix Dürrwald <mplabs@mplabs.de>
2021-07-19 11:19:26 +0300

    Add endpoint configuration to support custom URLs.

--- src/plugin-options.js
+++ src/plugin-options.js
@@ -7,12 +7,14 @@ export const schema = yup.object().shape({
       accessKeyId: yup.string(),
       secretAccessKey: yup.string(),
       sessionToken: yup.string(),
+      endpoint: yup.string(),
       region: yup.string(),
     })
     .default(() => ({
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       sessionToken: process.env.AWS_SESSION_TOKEN,
+      endpoint: process.env.AWS_DOMAIN,
       region: process.env.AWS_REGION,
     })),
   buckets: yup.array().required(),
